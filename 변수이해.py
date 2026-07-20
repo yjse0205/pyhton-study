8ce694dcf6001ff4bde5349a3b6aa4f5ee9e7604
yjse0205 <kikimi0205@gmail.com>
2026-07-20 10:56:10 +0900

    Add files via upload

---
+++ 변수이해.py
@@ -0,0 +1,16 @@
+//변수 이해   //한줄씩 입력  //var는 변수
+
+boolVar, intVar, floatVar, strVar = True, 0, 0.0, ""
+//이게 변수만들어준거
+type(boolVar), type(intVar), type(floatVar), type(strVar)
+
+//이게 만든 변수의 데이터 형식 나타나게 하는거
+
+a=True
+type(a)
+//이건 Bool형 형식이고 True, False만 저장가능 예를들어
+
+a=(100==100)
+b=(10>100)
+print(a,b)
+//이러면 결과가 True, False

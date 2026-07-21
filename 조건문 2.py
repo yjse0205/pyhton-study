98598c51f1b31b6d2147633239c9804f377472fa
yjse0205 <kikimi0205@gmail.com>
2026-07-21 15:11:00 +0900

    Add files via upload

---
+++ 조건문 2.py
@@ -0,0 +1,23 @@
+##if~elif~else
+
+score=int(input("점수입력: "))
+
+if score>=90:
+    print("A")
+elif score >=80:
+    print("B")
+elif score >=70:
+    print("C")
+elif score >=60:
+    print("D")
+else:
+    print("F")
+
+print("학점입니다.")
+
+##리스트
+fruit=['사과', '배', '딸기', '포도']
+print(fruit)
+
+fruit.append('귤')
+print(fruit)
